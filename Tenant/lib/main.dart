13d134f92e3fe6d33411f00691916455c6d77c5a
ArbSoftware <satabhishar@arbsoft.comm>
2023-06-21 16:02:34 +0530

    modified

--- Tenant/lib/main.dart
+++ Tenant/lib/main.dart
@@ -5,14 +5,14 @@ import 'package:tenant/screens/login.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); //---for mobile
-  await Firebase.initializeApp(
+  /*await Firebase.initializeApp(
     options: FirebaseOptions(
       apiKey: "AIzaSyAS5alN0XMgU9ofvrCxEDqtryX4Ghq6OIo", // Your apiKey
       appId: "1:436590148667:web:16a0215d002a334f80701e", // Your appId
       messagingSenderId: "436590148667", // Your messagingSenderId
       projectId: "dailyrecords-85331", // Your projectId
     ),
-  ); //---for web//
+  );*/ //---for web//
 
   runApp(const MyApp());
 }
